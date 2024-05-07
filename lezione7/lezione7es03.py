0f8d6a5505d2258a2137b537c7d556eefbf896f9
arcman1510 <arcman1510@gmail.com>
2024-05-07 18:49:05 +0200

    Svolto es03 lezione 7

--- lezione7/lezione7es03.py
+++ lezione7/lezione7es03.py
@@ -1,14 +1,27 @@
 class Animal:
 
-    def __init__(ani, name: str, legs: int):
-        ani.name: str = name
-        ani.legs: str = legs
+    def __init__(self, name: str, legs: int):
+        self.name: str = name
+        self.legs: str = legs
 
-spider = Animal("Spider", 8)
-rat = Animal("Rat.", 3)
+    spider = Animal("Spider", 8)
+    rat = Animal("Rat.", 3)
+    print(f'Nome ={spider.name},  Legs = {spider.legs}')
+    print(f'Nome ={rat.name},  Legs = {rat.legs}')
 
-print(f'Nome ={spider.name},  Legs = {spider.legs}')
-print(f'Nome ={rat.name},  Legs = {rat.legs}')
+    rat.legs = 4
+    print(f'Legs = {rat.legs}')
 
-rat.legs = 4
-print(f'Legs = {rat.legs}')
+    def set_legs(self, new_legs: int):
+        if new_legs >= 0:
+            self.legs
+        else:
+            self.legs = 0
+
+    def get_legs(self) -> int:
+            return self.legs
+    
+    def __str__(self) -> str:
+        return f'Name: {self.name}, Legs: {self.legs}'
+    
+   
