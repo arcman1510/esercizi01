ecc3fa139490cc57a0d4826f81ecb63891997936
arcman1510 <arcman1510@gmai.com>
2024-05-30 13:33:15 +0200

    Compilazione classe Film

---
+++ lezione11/ExerL11-01/film.py
@@ -0,0 +1,8 @@
+class Film:
+       def __init__(self, id: str, titolo: str, duratam: int):
+        self.id: str = id
+        self.titolo: str = titolo
+        self.duratam: int = duratam
+
+        def __str__(self) -> str:
+            return f'Id Film(id={self.id}, titolo ={self.titolo}, durata in minuti ={self.seats})'
