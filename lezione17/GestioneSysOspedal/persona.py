c5754787c96f598b8840282510226b0b009e1932
arcman1510 <arcman1510@gmai.com>
2024-06-14 12:24:46 +0200

    Creazione classe persona

---
+++ lezione17/GestioneSysOspedal/persona.py
@@ -0,0 +1,7 @@
+class Persona:
+
+    def __init__(self, first_name: str, last_name: str, age: int) -> None:
+        self.first_name: str = first_name
+        self.last_name: str = last_name
+        self.age: float = age
+        
