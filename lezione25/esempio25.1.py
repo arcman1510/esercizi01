872071b1134ce32fbeaca27a8b8dcdb87c3d1f41
arcman1510 <arcman1510@gmai.com>
2024-07-10 10:28:31 +0200

    Bubble Sort v2 MP

--- lezione25/esempio25.1.py
+++ lezione25/esempio25.1.py
@@ -1,6 +1,24 @@
 from multiprocessing import Process
 import time
 
+def bubble_sort_v2():
+
+    from random import randint
+
+    x =[randint(0, 1000) for _ in range(50000)]
+
+    ho_fatto_swap: bool = True
+    for i in range(len(x)):
+        for j in range(len(x) - i - 1):
+            if x[j] > x[j+1]:
+                # swap(x[j], x[j+1])
+                ho_fatto_swap = False
+                temp: int = x[j]
+                x[j] = x[j+1]
+                x[j+1] = temp
+            if ho_fatto_swap:
+                break
+
 def sleep():
 
     print(f"Sono nella funzione")
@@ -20,7 +38,6 @@ if __name__ == "__main__":
     t2.start()
     t1.join
     t2.join
-    time.sleep(45)
     toc: float = time.time()
     time_elapsed: float = toc - tic
 
