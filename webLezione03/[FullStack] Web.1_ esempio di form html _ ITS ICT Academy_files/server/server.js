a514f02a5027c7d671e0c7ac90bcfbf061392b3b
arcman1510 <arcman1510@gmai.com>
2024-07-23 10:49:16 +0200

    gestisciDataForm

---
+++ webLezione03/[FullStack] Web.1_ esempio di form html _ ITS ICT Academy_files/server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+app.use(cors());
+var iPortaTcp = 4201;
+var sIpAddress = "127.0.0.1"
+app.listen(iPortaTcp,sIpAddress, () => console.log('API is running on http://' + sIpAddress +
+    ':' + iPortaTcp));
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//pagina di invio della form
+app.get('/formRegistrazione', (req, res) => {
+    console.log("Mi hai chiesto la form di registrazione");
+    res.sendFile("formSemplice.html", { root: './htdoc' });
+    });
+
+//pagina di gestione dei dati della form se il metodo è GET
+app.get('/gestisciDatiForm', (req, res) => {
+    console.log(req.query.fname);
+    res.send("<html>Buona serata a tutti</html>");
+    });
